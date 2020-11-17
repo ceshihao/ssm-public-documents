fdcd356c0b7d96fa43f39e1dbd32525935fdecf5
Zheng Dayu <davidzheng23@gmail.com>
2020-11-17 17:20:57 +0800

    add github actions to update ssm documents

--- sync-documents.sh
+++ sync-documents.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 aws ssm list-documents > list-documents.json
 cat list-documents.json | jq -cr '.DocumentIdentifiers[].Name' | xargs -n1 sh -c 'aws ssm get-document --name $1 | jq ".Content|fromjson" > "documents/$1.json"' sh
