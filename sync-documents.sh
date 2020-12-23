9eed99063e5ef1f117bdee3cda12fb19c6ba7d85
Zheng Dayu <davidzheng23@gmail.com>
2020-12-23 20:12:01 +0800

    sleep to avoid ThrottlingException

--- sync-documents.sh
+++ sync-documents.sh
@@ -3,4 +3,4 @@
 set -e
 
 aws ssm list-documents > list-documents.json
-cat list-documents.json | jq -cr '.DocumentIdentifiers[].Name' | xargs -n1 sh -c 'aws ssm get-document --name $1 | jq ".Content|fromjson" > "documents/$1.json"' sh
+cat list-documents.json | jq -cr '.DocumentIdentifiers[].Name' | xargs -n1 sh -c 'sleep 1 && aws ssm get-document --name $1 | jq ".Content|fromjson" > "documents/$1.json"' sh
