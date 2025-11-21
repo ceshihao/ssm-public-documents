5c1f195c06ab83fd67e12df9dd369b88590fad15
Zheng Dayu <davidzheng23@gmail.com>
2025-11-21 15:53:40 +0800

    Update sync-documents.sh to filter SSM documents by owner 'Amazon'
    Change-Id: I4632a7511bc01bbbe35a787fa2247901c8c25609
    Co-developed-by: Cursor <noreply@cursor.com>

--- sync-documents.sh
+++ sync-documents.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-aws ssm list-documents > list-documents.json
+aws ssm list-documents --filters Key=Owner,Values=Amazon > list-documents.json
 cat list-documents.json | jq -cr '.DocumentIdentifiers[].Name' | xargs -n1 sh -c 'sleep 1 && aws ssm get-document --name $1 | jq ".Content|fromjson" > "documents/$1.json"' sh
